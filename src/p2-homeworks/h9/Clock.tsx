c7bd3405e4c061825cd42bc11d6663afe0f65c35
Shastin Maksim <shastin_maksim@mail.ru>
2022-02-08 11:16:31 +0300

    add hw9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -3,48 +3,54 @@ import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<string>()
     const [show, setShow] = useState<boolean>(false)
+    const [showDate, setShowDate] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date().toLocaleTimeString())
         }, 1000)
         setTimerId(id)
+        setShow(true)
+
     }
 
     const onMouseEnter = () => {
-        // show
+        setShowDate(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShowDate(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+
+    const stringTime = new Date().toLocaleTimeString() // fix with date
+    const stringDate = `${new Date().getFullYear()}-${new Date().getMonth()}-${new Date().getDate()}` // fix with date
 
     return (
-        <div>
-            <div
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-            >
-                {stringTime}
-            </div>
+            <div>
 
-            {show && (
                 <div>
-                    {stringDate}
-                </div>
-            )}
+
+                    <div
+                        onMouseEnter={onMouseEnter}
+                        onMouseLeave={onMouseLeave}>
+                        <div>
+                            {show && stringTime}
+                            <div>
+                                {showDate && stringDate}
+                            </div>
+                        </div>
+                    </div>
+
 
             <SuperButton onClick={start}>start</SuperButton>
             <SuperButton onClick={stop}>stop</SuperButton>
-
+                </div>
         </div>
     )
 }
